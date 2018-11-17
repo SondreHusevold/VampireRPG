e81897ea3d142e4b2f9a9da3cf54e2e9b1699a5e
sondrehusev@gmail.com <sondrehusev@gmail.com>
2018-11-17 14:51:02 +0100

    VampireRPG is Complete. Thank you for playing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,4 @@ import { MainframeComponent } from './mainframe/mainframe.component';
 })
 export class AppComponent {
   title = 'Vampire the Masquerade: Miami';
-
-    constructor(private router: Router) {
-        router.events.subscribe( (event: Event) => {
-            if (event instanceof NavigationStart) {
-            	let test = Math.floor(Math.random() * Math.floor(100));
-            	if(test < 10 && (new Date() === new Date(2018, 11, 16, 23, 0, 0))) {
-    				window.location.href = 'https://sondrehusevold.github.io/CyberpunkRPG/';
-            	}
-			}
-        });
-	};
 }
